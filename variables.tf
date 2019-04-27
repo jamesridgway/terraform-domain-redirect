64e133c9505db3bff98f0950c2903ab9d78b1940
James Ridgway <myself@james-ridgway.co.uk>
2019-04-27 17:19:01 +0100

    Added variables

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "source_domain" {
+	type = "string"
+	description = "Source domain"
+}
+variable "destination_address" {
+  type = "string"
+  description = "Destination address (including protocol and host e.g. https://www.example.com)"
+}
